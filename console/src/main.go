f99c04da0f28ced6575b1e6784fabde0bba97cab
funnsam <funnsam8@gmail.com>
2023-06-23 17:25:21 +0800

    better ui for console

--- console/src/main.go
+++ console/src/main.go
@@ -7,14 +7,11 @@ import (
 	"fmt"
 )
 
-func center(p tview.Primitive, width, height int) tview.Primitive {
-	return tview.NewFlex().
-		AddItem(nil, 0, 1, false).
-		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(nil, 0, 1, false).
-		AddItem(p, height, 1, true).
-		AddItem(nil, 0, 1, false), width, 1, true).
-		AddItem(nil, 0, 1, false)
+func center(p tview.Primitive) tview.Primitive {
+	return tview.NewGrid().
+		SetColumns(0, -4, 0).
+		SetRows(0, -4, 0).
+		AddItem(p, 1, 1, 1, 1, 0, 0, true)
 }
 
 func main() {
@@ -28,18 +25,18 @@ func main() {
 	pages := tview.NewPages()
 	list := tview.NewList()
 	details := tview.NewTextView()
-	_, _, w, h := pages.GetInnerRect()
 
 	list.SetHighlightFullLine(true)
 	list.SetMainTextStyle(tcell.StyleDefault.Attributes(tcell.AttrBold))
 	list.SetMainTextColor(tcell.ColorWhite)
-	list.SetSecondaryTextColor(tcell.NewHexColor(0xBFBFBF))
+	list.SetSecondaryTextColor(tcell.NewHexColor(0xAFAFAF))
 
 	details.SetBorder(true)
 	details.SetDynamicColors(true)
+	details.SetTitleAlign(tview.AlignLeft)
 
 	pages.AddPage("CPU List", list, true, true)
-	pages.AddPage("Details", center(details, w*w/h, h*w/h), true, false)
+	pages.AddPage("Details", center(details), true, false)
 
 	for _, v := range *db {
 		list.AddItem(v.Name, fmt.Sprintf("%s, %.2f Hz", v.Author, v.Speed), 0, nil)
@@ -55,11 +52,10 @@ func main() {
 
 		ns := ""
 		ne := ""
-		fs := "[DarkSlateGray::b]"
+		fs := "[#AFAFAF::]"
 		fe := "[-:-:-]"
 
-		fmt.Fprintf(w, "%sName:%s %s%s%s\n", ns, ne, fs, d.Name, fe)
-		fmt.Fprintf(w, "%sAuthor:%s %s%s%s\n", ns, ne, fs, d.Author, fe)
+		details.SetTitle(fmt.Sprintf("%s (by %s)", d.Name, d.Author))
 		fmt.Fprintf(w, "%sSpeed:%s %s%.2f Hz%s\n", ns, ne, fs, d.Speed, fe)
 		if d.Pipeline != 0 {
 			fmt.Fprintf(w, "%sPipeline stages:%s %s%d%s\n", ns, ne, fs, d.Pipeline, fe)
